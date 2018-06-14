0b7317eb1a1a5484295f6d409497523848428018
Jacob Levernier <jlevern@upenn.edu>
2018-06-14 19:21:57 -0400

    Added two-file Shiny app skeleton, after determining that Shiny can meet the technical goals (async. requests with futures package, likely a 1-second load time (with pre-caching), separate server logic, and dynamic display).

---
+++ server.R
@@ -0,0 +1,16 @@
+## Server logic for the Shiny web application.
+## This app can be run locally from within RStudio using
+## its "Run App" button.
+
+## Load libraries ----------------------------------------------------------
+
+library(shiny)
+
+## Define server logic -----------------------------------------------------
+
+## Define server-side logic
+shinyServer(function(input, output) {
+   
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,27 @@
+## User Interface for the Shiny web application.
+## This app can be run locally from within RStudio using
+## its "Run App" button.
+
+## Load libraries ----------------------------------------------------------
+
+library(shiny)
+
+
+## Define UI ---------------------------------------------------------------
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Cohort Overview"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       
+    ),
+    
+    mainPanel(
+       
+    )
+  )
+))
