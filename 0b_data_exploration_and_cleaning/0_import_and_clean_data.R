c0610789c789f978716871974f8dcdef5bf60ca3
Jacob Levernier <jlevern@upenn.edu>
2018-06-14 20:15:31 -0400

    Added steps to import the dataset, clean it by resolving age columns and deduplicating, and doing some initial exploration.

--- 0b_data_exploration_and_cleaning/0_import_and_clean_data.R
+++ 0b_data_exploration_and_cleaning/0_import_and_clean_data.R
@@ -4,12 +4,73 @@
 
 source('0a_helper_functions/check_packages.R')
 
+check_packages('magrittr')
 check_packages('tidyverse')
 
-# Import Dataset ----------------------------------------------------------
+
+# Import the data ---------------------------------------------------------
 
 ## We have two cohorts, each in a discrete directory, and each with two
 ## CSV files to be joined.
 
+dataset <- list.dirs(
+  path = 'data',
+  full.names = TRUE,
+  recursive = TRUE
+) %>% 
+  magrittr::extract(-1) %>%  ## Remove the 'data' directory itself
+  purrr::map_dfr(function(x) {
+    ## Extract the cohort name from the filepath:
+    cohort_name <- x %>% 
+      stringr::str_split('/', simplify = TRUE) %>% 
+      magrittr::extract(2)
+    
+    ## Read the patient data, and join it to the problems data,
+    ## recording source:
+    readr::read_csv(file.path(x, 'patients.csv')) %>% 
+      dplyr::mutate(source = cohort_name) %>% 
+      left_join(
+        readr::read_csv(file.path(x, 'problems.csv')),
+        by = 'pat_id'
+      )
+  })
 
- 
+# Clean the imported data -------------------------------------------------
+
+## Correct variable types:
+dataset %<>% 
+  dplyr::mutate(
+    sex = as.factor(sex),
+    ethnicity = as.factor(ethnicity),
+    source = as.factor(source),
+    icd9_code = as.factor(icd9_code)
+  )
+
+## Remove duplicate rows:
+dataset %<>% 
+  dplyr::distinct()
+
+## If I understand correctly, and resolved_age is to be used in place
+## of noted_age, combine the two into a column:
+dataset %<>% 
+  dplyr::mutate(
+    age = ifelse(!is.na(resolved_age), resolved_age, noted_age)
+  )
+
+## Examine the imported and cleaned dataset:
+# dplyr::glimpse(dataset)
+# View(dataset)
+
+# Explore the dataset -----------------------------------------------------
+
+dataset %>% 
+  dplyr::group_by(source, sex, ethnicity, race) %>% 
+  dplyr::summarize(
+    age_mean = mean(age, na.rm = FALSE),
+    age_sd = sd(age, na.rm = FALSE)
+    # icd9_code
+  )
+
+
+
+
